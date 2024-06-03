e0d17ec93c0359ffcd8ccb14a3d1f8efced5ff7b
Piyush-linux <piyushpatil98@protonmail.com>
2024-06-03 20:50:28 +0530

    workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  app: {
+    baseURL: '/astraos/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   css: ['~/assets/shepherd.css'],
   modules: [
     '@nuxtjs/tailwindcss',
